db48adb03779f77e173633d04f59f87910a94c2c
salihu <57968062+salihuabubakar@users.noreply.github.com>
2022-12-19 21:55:40 +0100

    login page

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
-let h1 = document.getElementById("h1").innerHTML = "Student Dashboard";
-h1 = document.getElementById("h1").style.color = "red";
+// Login 
+let matricNo = document.getElementById("matricNo");
+let password = document.getElementById("password");
+let errorMsg = document.getElementById("error");
+let loginForm = document.getElementById("loginForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  if(matricNo.value === "") {
+    errorMsg.innerHTML = "Matric Number is required";
+  } else if(password.value === "") {
+    errorMsg.innerHTML = "Password is required";
+  } else if(password.value.length < 6) {
+    errorMsg.innerHTML = "Password must be atleast 6 characters long";
+  }else {
+    errorMsg.innerHTML = "processing...";
+    console.log("successfully logged in");
+  }
+})
